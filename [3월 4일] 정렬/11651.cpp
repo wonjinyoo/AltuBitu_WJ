7752ffe6345706c3764ca4f41ee3a28508d0f5dc
wonjinyoo <96529879+wonjinyoo@users.noreply.github.com>
2022-03-10 19:47:26 +0900

    Update 11651.cpp
    말씀해주신대로 구조체 대신 pair를 사용하여 수정하였습니다!

--- [3월 4일] 정렬/11651.cpp
+++ [3월 4일] 정렬/11651.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
-struct point {
-	int x, y;
-};
+typedef pair<int, int> pt; // 구조체 대신 pair 이용 (utility 헤더), first second로 접근
 
-bool cmp(const point& a, const point& b) {
-	if (a.y != b.y) {
-		return a.y < b.y;
+bool cmp(const pt& a, const pt& b) {
+	if (a.second != b.second) {
+		return a.second < b.second;
 	}
-	return a.x < b.x;
+	return a.first < b.first;
 }
 
 int main() {
-	int n;
+	int n, a, b;
 	cin >> n;
-	vector<point> p(n);
+	vector<pt> p;
 	for (int i = 0; i < n; i++) {
-		cin >> p[i].x >> p[i].y;
+		cin >> a >> b;
+		p.push_back(pt(a, b));  // 이 과정이 헷갈렸음.. 벡터에 페어를 담는 게 익숙하지가 않음.. 
 	}
-	
+
 	sort(p.begin(), p.end(), cmp);
 
 	for (int i = 0; i < n; i++) {
-		cout << p[i].x << ' ' << p[i].y << '\n';
+		cout << p[i].first << ' ' << p[i].second << '\n';
 	}
 	return 0;
 }
+
