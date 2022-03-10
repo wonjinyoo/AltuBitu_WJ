50c28008f6ae9c255b9d839b56c5af70577c7d0d
wonjinyoo <96529879+wonjinyoo@users.noreply.github.com>
2022-03-10 20:35:56 +0900

    Update 1758.cpp
    메인 함수 내에 있던 식을 함수로 빼서 작성하였고요,  int* 형태로 동적할당 하는 대신 vector를 이용하여 코드 수정하였습니다!
    그런데 다른 질문이 있습니다! 20행에서 'vector<int> tip'와 같이 개수를 직접 지정해서 적어주지 않으면 비주얼 스튜디오에서 'vector subscript out of range' 라는 에러가 뜨더라고요.. 직접 개수를 지정해줘야만 한다면 동적할당의 의미가 없어지는 것 같은데, 뭐 때문에 그런 걸까요..? 아니면 그것만의 문제가 아니었던 걸까요?😭

--- [3월 4일] 정렬/1758.cpp
+++ [3월 4일] 정렬/1758.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	int c;
-	long long sum = 0;
-	cin >> c;
-	int* tip = new int[c];
+void tipcal(int n, vector<int> v) {
+	long long s = 0;
+	for (int i = 0; i < n; i++) {
+		int rtip = v[i] - i;
+		if (rtip < 0) rtip = 0;
+		s += rtip;
+	}
+	cout << s;
+}
 
-	for (int i = 0; i < c; i++) { //등수= i+1
+int main() {
+	int c, a;
+	cin >> c;
+	vector<int> tip(c);  // 벡터의 개수를 c로 지정해줘야지 에러가 안난다...?
+
+	for (int i = 0; i < c; i++) { // 등수= i+1
 		cin >> tip[i];
 	}
 
-	sort(tip, tip + c, greater<>());
+	sort(tip.begin(), tip.end(), greater<>());
+	tipcal(c, tip);
 
-	for (int i = 0; i < c; i++) {
-		int rtip = tip[i] - i;
-		if (rtip < 0) rtip = 0;
-		sum += rtip;
-	}
-
-	cout << sum;
-	
-	delete tip;
 	return 0;
 }
