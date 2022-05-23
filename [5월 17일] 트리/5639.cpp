b15d90188c178a1d1e1ef76c0e66b7e22a156a3c
wonjinyoo <96529879+wonjinyoo@users.noreply.github.com>
2022-05-24 02:22:28 +0900

    Update 5639.cpp

--- [5월 17일] 트리/5639.cpp
+++ [5월 17일] 트리/5639.cpp
@@ -1,61 +1,69 @@
-
+//
 // [5639] 이진 검색 트리
-//		-> 틀린 코드입니다 .. 
-//		-> 내용 질문이 좀 많은데 주석 읽어주시면 감사하겠습니다 !! :)
+//		-> 맵 말고 구조체, 포인터 이용해서 트리 생성 !!
 
 #include <iostream>
-#include <map>
+
 using namespace std;
 
-// 루트 노드, left, right값
-map<int, pair<int, int>> tree;
+// 구조체 : 루트 노드, left, right값 -> 계속 노드로 연결
+struct Node {
+	int data;
+	Node* left;
+	Node* right;
+};
 
-
-// 이게.. 맞나요..?
-void postorder(int v) {
-	
-	// '정수형 = NULL' 이렇게 쓸 수 있는 건가요..?
-	if (v == NULL) {
-		return;
+// 트리 만드는 함수 : 리턴형 void로 하면 까다롭..
+Node* makeTree(Node* tree, int x) {
+	// 맨 처음거 메인 함수에서 초기화했다고 이 부분 안 넣어주면 안됐음 -> left, right 노드들은 안 채워졌을 거니까!
+	if (tree == NULL) {
+		tree = new Node();
+		tree->data = x;
+		tree->left = tree->right = NULL;
 	}
-	postorder(tree[v].first);
-	postorder(tree[v].second);
-	cout << v << '\n';
+
+	// 'else' if..
+	else if (x <= tree->data) {
+		tree->left = makeTree(tree->left, x);
+	}
+	else tree->right = makeTree(tree->right, x);
+
+	return tree;
+}
+
+// 후위순회
+void postorder(Node* tree) {
+	if (tree->left != NULL) {
+		postorder(tree->left);
+	}
+	if (tree->right != NULL) {
+		postorder(tree->right);
+	}
+
+	cout << tree->data << '\n';
 }
 
 
 int main() {
 	int root, x;
-	
-	// 맨 처음 입력값을 제일 부모노드라고 생각해서 먼저 하나 입력 받고
-	// 얘를 기준으로 크기를 비교하면서 트리를 채워나간다고 생각했습니다
+
 	cin >> root;
 
+	// 맨 처음 초기화 -> 루트노드 설정
+	Node* tree = new Node();
+	tree->data = root;
+	tree->left = tree->right = NULL;
 
-	// 트리를 하나씩 채워나갈 때 비교해주기 위한 변수
-	int comp = root;
-
-
-	// 입력이 언제 종료될지 모를 때 어떻게 코드를 작성하면 좋을지 모르겠습니다..
-	// 다른 분이 올려주신 질문 참고해서 cin.eof() 검색해서 사용해보았는데도 아무 출력이 안 나왔습니다..
-	// 여기뿐만이 아니고 다른 데에서도 틀린 것 같은데 어디가 문젠지 잘 모르겠습니다..!
-
+	// ctrl + z 입력해야 !
 	while (cin >> x) {
 		if (x == EOF) break;
-
-		// 이런 식으로 트리를 생성해도 되나요..?
-		if (x < comp) {
-			tree[comp].first = x;
-		}
-		if (x > comp) {
-			tree[comp].second = x;
-		}
-		comp = x;
+		
+		// 계속 트리 갱신
+		tree = makeTree(tree, x);
 	}
 
-
-	// 함수 실행이 안 되고 있는 건지 어쩐건지 .. 뭐가 문젤까요..
-	postorder(root);
+	// 후위순회
+	postorder(tree);
 
 	return 0;
 }
