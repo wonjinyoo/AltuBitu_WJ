5556d62b00a508cfb21b2d9c73811165eeaa69d3
wonjinyoo <96529879+wonjinyoo@users.noreply.github.com>
2022-05-23 23:17:37 +0900

    Update 1068.cpp

--- [5월 17일] 트리/1068.cpp
+++ [5월 17일] 트리/1068.cpp
@@ -1,77 +1,91 @@
+//
 // [1068] 트리
-//		-> 틀린 코드인데요.. 대략적으로 이 아이디어 자체는 맞을까요..?
-//		  힌트 주시면 수정하겠습니다!
+//   -> 말씀해주신대로 수정 최대한 해보았습니다..! 알려주신대로 하니까 코드 방향은 확실히 알겠어요!! 
+//		그래도 구현하는 데 있어서 여전히 모르는 부분이 있어서요 ㅠㅠ 흑흑 
 
 #include <iostream>
 #include <map>
 #include <vector>
-#include <set>
 
 using namespace std;
 
-// 트리를 맵으로 생성 : 각각 부모노드 번호, set에 자식 노드 담을 용도
-map<int, set<int>> tree;
+// 이차원 벡터로 -> 인덱스가 부모노드, 노드 번호는 벡터로 저장
+vector<vector<bool>> tree;
 
+// 방문체크용
+vector<bool> visited;
 
-// 노드 삭제하기 (트리, 삭제할 노드 번호)
-void deleteNode(map<int, set<int>> tree, int del) {
-	vector<int> check;
-	
-	// 자식 노드가 없을 때 리턴
-	if (tree[del].size() == 0) {
-		return;
-	}
-	
-	// 삭제할 노드의 자식노드들 번호를 check 벡터에 넣어준다
-	for (auto it = tree[del].begin(); it != tree[del].end(); it++) {
-		check.push_back(*it);
-	}
-
-	// 노드 삭제
-	tree.erase(del);
-
-	// 삭제한 노드의 자식 노드들도 좌르륵 삭제하기..
-	for (int i = 0; i < check.size(); i++) {
-		deleteNode(tree, check[i]);
-	}
-}
-
+// 리프노드 담는..
+vector<int> leaf;
 
 // 리프노드 개수 구하는 함수
-int leafNode(map<int, set<int>> tree) {
-	int cnt = 0;
-	
-	// 맵 순회하면서 자식노드가 0개이면 cnt를 1 늘려주는 방식으로
-	for (auto it = tree.begin(); it != tree.end(); it++) {
-		if (it->second.size() == 0) {
-			cnt++;
-		}
+// 이 부분을 잘 모르겠습니다.. dfs탐색 부분이 아직 약한 것 같습니다..ㅜ
+void dfsLeafNode(int n, vector<vector<bool>> &tree, int curr, int del) {
+	if (visited[curr]) {
+		return;
 	}
 
-	return cnt;
+	visited[curr] = true;
+	
+	
+	for (int next = 0; next < n; next++) {
+		
+		// 자식노드 아닌 것의 개수를 세려고 설정은 했습니다..
+		int cnt = 0;
+		
+		if (visited[next]) continue;
+		
+		if (next == del) {
+			tree[curr][next] = false;
+		}
+		
+
+		if (tree[curr][next]) {
+			dfsLeafNode(n, tree, next, del);
+		}
+		// next가 curr의 자식 노드가 아니면 cnt를 하나 늘려주기..?
+		else cnt++;
+
+		// 마지막까지 자식노드가 하나도 없으면 리프노드 벡터에 넣어주려는 용도입니다..
+		if (next == n - 1 && cnt == n) leaf.push_back(curr);
+	}
+
+	// Q. 제거할 노드를 제외하고 탐색할 수 있는 조건문을 어떻게 추가하면 좋을지 잘 모르겠습니다..ㅜㅜ
 }
+
 
 int main() {
 	int n, node, del;
-
+	int root;
+	
 	cin >> n;
+
+	// 0 ~ n-1까지 노드 있음, false로 방문 초기화
+	visited.assign(n, false);
+
+	tree.assign(n, vector<bool>(n, false));
 
 	// 입력 받기
 	for (int i = 0; i < n; i++) {
 		cin >> node;
-		
-		// i번쨰 노드에 부모노드가 없으면 그대로 냅둔다..(?)
+
+		// 루트노드 root에 저장!
 		if (node == -1) {
+			root = i;
 			continue;
 		}
-		// 부모 노드 없으면 트리 만들어주기..
-		tree[node].insert(i);
+		// 부모 노드가 node이고, i를 자식으로 넣어주기
+		else {
+			tree[node][i] = true;
+		}
 	}
 
 	cin >> del;
-	deleteNode(tree, del);
 
-	cout << leafNode(tree);
-	
+	dfsLeafNode(n, tree, root, del);
+
+	cout << leaf.size();
+
+
 	return 0;
 }
