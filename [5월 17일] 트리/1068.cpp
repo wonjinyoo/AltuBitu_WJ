1bc32a7ff10271f6755b3f445d1eb0ff3652dabd
wonjinyoo <96529879+wonjinyoo@users.noreply.github.com>
2022-05-26 10:58:18 +0900

    Update 1068.cpp

--- [5월 17일] 트리/1068.cpp
+++ [5월 17일] 트리/1068.cpp
@@ -1,69 +1,47 @@
 //
 // [1068] 트리
-//   -> 말씀해주신대로 수정 최대한 해보았습니다..! 알려주신대로 하니까 코드 방향은 확실히 알겠어요!! 
-//		그래도 구현하는 데 있어서 여전히 모르는 부분이 있어서요 ㅠㅠ 흑흑 
+//  
 
 #include <iostream>
-#include <map>
 #include <vector>
 
 using namespace std;
 
 // 이차원 벡터로 -> 인덱스가 부모노드, 노드 번호는 벡터로 저장
-vector<vector<bool>> tree;
+vector<vector<int>> tree;
 
-// 방문체크용
-vector<bool> visited;
 
-// 리프노드 담는..
-vector<int> leaf;
-
-// 리프노드 개수 구하는 함수
-// 이 부분을 잘 모르겠습니다.. dfs탐색 부분이 아직 약한 것 같습니다..ㅜ
-void dfsLeafNode(int n, vector<vector<bool>> &tree, int curr, int del) {
-	if (visited[curr]) {
-		return;
+// 리프노드 개수 구하는 함수 (현재 루트노드, 삭제할 노드 매개변수)
+int dfsLeafNode(int curr, int del) {
+	// 자신이 곧 삭제될 노드면 -> 0
+	if (curr == del) {
+		return 0;
 	}
 
-	visited[curr] = true;
-	
-	
-	for (int next = 0; next < n; next++) {
-		
-		// 자식노드 아닌 것의 개수를 세려고 설정은 했습니다..
-		int cnt = 0;
-		
-		if (visited[next]) continue;
-		
-		if (next == del) {
-			tree[curr][next] = false;
-		}
-		
 
-		if (tree[curr][next]) {
-			dfsLeafNode(n, tree, next, del);
-		}
-		// next가 curr의 자식 노드가 아니면 cnt를 하나 늘려주기..?
-		else cnt++;
-
-		// 마지막까지 자식노드가 하나도 없으면 리프노드 벡터에 넣어주려는 용도입니다..
-		if (next == n - 1 && cnt == n) leaf.push_back(curr);
+	// 자식이 없거나 or 자식이 하나로 유일한데 그게 곧 삭제될 노드라면
+	if (tree[curr].empty() || tree[curr].size() == 1 && tree[curr][0] == del) {
+		return 1;
 	}
 
-	// Q. 제거할 노드를 제외하고 탐색할 수 있는 조건문을 어떻게 추가하면 좋을지 잘 모르겠습니다..ㅜㅜ
+	int cnt = 0;
+
+	// 자식노드들 따라 내려가면서 cnt값 갱신
+	for (int i = 0; i < tree[curr].size(); i++) {
+		cnt += dfsLeafNode(tree[curr][i], del);
+	}
+
+	return cnt;
 }
 
 
 int main() {
-	int n, node, del;
-	int root;
-	
+	int n, node, del, root;
+
 	cin >> n;
 
-	// 0 ~ n-1까지 노드 있음, false로 방문 초기화
-	visited.assign(n, false);
-
-	tree.assign(n, vector<bool>(n, false));
+	// bool형보다는 int형으로!
+	tree.assign(n, vector<int>(0));
 
 	// 입력 받기
 	for (int i = 0; i < n; i++) {
@@ -76,16 +54,13 @@ int main() {
 		}
 		// 부모 노드가 node이고, i를 자식으로 넣어주기
 		else {
-			tree[node][i] = true;
+			tree[node].push_back(i);
 		}
 	}
 
 	cin >> del;
 
-	dfsLeafNode(n, tree, root, del);
-
-	cout << leaf.size();
-
+	cout << dfsLeafNode(root, del);
 
 	return 0;
 }
